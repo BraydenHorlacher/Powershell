e27daa2e7f485adad3c97ec13a49c198861e84f6
Sagaichi <92725060+Sagaichi@users.noreply.github.com>
2022-02-07 00:50:37 +1300

    Changed to run AutomationScript if user doesnt want to continue

--- CSV User Creation Script.ps1
+++ CSV User Creation Script.ps1
@@ -4,7 +4,7 @@ pause
 
 $Continue = Read-Host -Prompt "Do you want to continue running this script?"
 if ($continue -eq "Y" -eq "y") {$null + "cls"}
-if ($Continue -eq "N" -eq "n") {./AutomationScripts.ps1}
+if ($Continue -eq "N" -eq "n") {Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sagaichi/Powershell/main/AutomationScript.ps1'))}
 
 #Imports the Active Directory to allow the script to run
 #requires -module ActiveDirectory   
