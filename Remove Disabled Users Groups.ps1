82aecafe6b0264463599fb2fcdf21fe3ae88ec9e
BraydenHorlacher <sagaichi1@gmail.com>
2022-05-13 12:26:22 +1200

    Fixed issues

--- Remove Disabled Users Groups.ps1
+++ Remove Disabled Users Groups.ps1
@@ -23,8 +23,8 @@ if($ExceptGroup = "") {
 $users = Get-ADUser -SearchBase $OU -Filter {Enabled -eq $False}
 
 $Confirm = Read-Host -Prompt "Do you want to manually check off each group removal for the users in this OU? [Y/N]"
-if ($Confirm -eq "y" -eq "Y")
-    foreach ($user in $users) {
+if ($Confirm -eq "y" -eq "Y"){
+    foreach ($user in $users){
         $UserDN = $user.DistinguishedName
         Get-ADGroup -LDAPFilter "(member=$UserDN)" | foreach-object {
             if ($_.name -ne $ExceptGroup) { 
@@ -34,7 +34,8 @@ if ($Confirm -eq "y" -eq "Y")
             }
         }
     }
-    
+}
+
 if ($Confirm -eq "n" -eq "N"){
     foreach ($user in $users) {
         $UserDN = $user.DistinguishedName
