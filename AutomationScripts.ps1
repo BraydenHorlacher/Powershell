210878223af7197de9f0ad7f71e8c9da834f78e0
Brayden Horlacher <92725060+BraydenHorlacher@users.noreply.github.com>
2022-05-19 11:53:39 +1200

    Added "Display all devices in AD" script

--- AutomationScripts.ps1
+++ AutomationScripts.ps1
@@ -32,6 +32,8 @@ I AM NOT LIABLE FOR ANY ISSUES CREATED BY THESE SCRIPTS! USE AT YOUR OWN RISK!
 
     6.) Folder Creation from CSV file.
 
+    7.) Display devices in AD 
+
     Exit.) Exits the script (Enter Q).
     
     Choice"
@@ -51,7 +53,8 @@ I AM NOT LIABLE FOR ANY ISSUES CREATED BY THESE SCRIPTS! USE AT YOUR OWN RISK!
     if ($Userchoice -eq 5) {Start-Process 'https://github.com/BraydenHorlacher/Powershell/blob/main/AutomaticWindowsUpdates'}
     #Creates Folders from a CSV File
     if ($Userchoice -eq 6) {Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BraydenHorlacher/Powershell/main/AutomaticFolderCreation%20-%20csv'))}
-    
+    #Displays all AD devices from user inputs
+    if ($Userchoice -eq 7) {Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BraydenHorlacher/Powershell/main/Display%20all%20devices%20in%20AD.ps1'))}
     
 # Game's
     #Number Game
