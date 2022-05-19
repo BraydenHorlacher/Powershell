4f48876012206f4d07fb04420fa68eee9144af22
Brayden Horlacher <92725060+BraydenHorlacher@users.noreply.github.com>
2022-05-19 13:30:28 +1200

    Script now outputs to .CSV file instead of a .txt file

--- Display all devices in AD.ps1
+++ Display all devices in AD.ps1
@@ -13,28 +13,19 @@ Clear-Host
 #requires -module ActiveDirectory
 
 # Var's
-$Confirm = Read-Host -Prompt "Do you want to search for Enabled or Disabled devices? or both? {[E] Enabled | [D] Disabled | [B] Both}"
+$Confirm = Read-Host -Prompt "Do you want to search for Enabled or Disabled devices? or both? | [E] Enabled | [D] Disabled | [B] Both |"
 $Log = Read-Host -Prompt "Would you like to create a log for all the devices? [Y/N]"
 
 # Code
 if($Log -eq "Y"){
     if($Confirm -eq "E" -eq "Enabled"){
-        Get-ADComputer -Filter {Enabled -eq $True} -Properties Description >> EnabledDevicesLog.txt
-        ForEach-Object{
-            Write-Output Get-ADComputer -Filter {Enabled -eq $True} -Properties Description >> EnabledDevicesLog.txt
-        }
+        Get-ADComputer -Filter {Enabled -eq $True} -Property * | Select-Object Name,OperatingSystem,ipv4Address,Enabled | Export-CSV ADcomputerslist.csv -NoTypeInformation -Encoding UTF8
     }
     if($Confirm -eq "D" -eq "Disabled"){
-        Get-ADComputer -Filter {Enabled -eq $False} -Properties Description >> DisabledDevicesLog.txt
-        ForEach-Object{
-            Get-ADComputer -Filter {Enabled -eq $False} -Properties Description >> DisabledDevicesLog.txt
-        }
+        Get-ADComputer -Filter {Enabled -eq $False} -Property * | Select-Object Name,OperatingSystem,ipv4Address,Enabled | Export-CSV ADcomputerslist.csv -NoTypeInformation -Encoding UTF8
     }
     if($Confirm -eq "B" -eq "Both"){
-        Get-ADComputer -Filter * -Properties Description >> DevicesLog.txt
-        ForEach-Object{
-            Get-ADComputer -Filter * -Properties Description >> DevicesLog.txt
-        }
+        Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,ipv4Address,Enabled | Export-CSV DeviceLog.csv -NoTypeInformation -Encoding UTF8
     }
 } else{
     if($Confirm -eq "E" -eq "Enabled"){
